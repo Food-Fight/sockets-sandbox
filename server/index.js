b72c70401516d6800e1c51da6de28ef643b04af3
Carter Robinson <cartertrobinson@gmail.com>
2018-06-05 13:11:29 -0500

    5

--- server/index.js
+++ server/index.js
@@ -18,6 +18,13 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/items', function (req, res) {
   items.selectAll(function(err, data) {
     if(err) {
